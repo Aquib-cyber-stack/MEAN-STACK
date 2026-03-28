ac2f2bf66cb5cce87d9359a5446c3bc7dae4d5ec
Aquib-cyber-stack <aquibs9999@gmail.com>
2026-03-28 10:20:08 +0530

    Update postsRepo.js

--- server/repos/postsRepo.js
+++ server/repos/postsRepo.js
@@ -1,131 +1,92 @@
-const Post = require("../models/Post.js");
+const postRepo = require('../repos/postsRepo');
+
+const createPost = async (postData) => {
+	try {
+		const newPost = await postRepo.createPost(postData);
+		return newPost;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
+}
 
 const getAllPosts = async (userId) => {
-    try {
-        const posts = await Post.find({}).populate('author', 'username imgUrl').sort({ createdAt: -1 });
-        const postsWithLikesStatus = posts.map(post => {
-            const isLiked = post.likedBy.includes(userId);
-            return { ...post._doc, isLiked };
-        })
-        return postsWithLikesStatus;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+	try {
+		const posts = await postRepo.getAllPosts(userId);
+		return posts;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
 }
 
 const getPostById = async (userId, postId) => {
-    try {
-        const post = await Post.findById(postId).populate('author', 'username imgUrl');
-        if (!post) {
-            throw new Error('Post not found');
-        }
-        if (post.likedBy.includes(userId)) {
-            post.isLiked = true;
-        }
-        return post;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+	try {
+		const post = await postRepo.getPostById(userId, postId);
+		return post;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
 }
 
 const getPostsByUser = async (userId) => {
-    try {
-        const posts = await Post.find({ author: userId }).populate('author', 'username imgUrl').sort({ createdAt: -1 });
-        const postsWithLikesStatus = posts.map(post => {
-            const isLiked = post.likedBy.includes(userId);
-            return { ...post._doc, isLiked };
-        })
-        return postsWithLikesStatus;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
-
-const createPost = async (postData) => {
-    try {
-        const post = new Post(postData);
-        await post.save();
-        return post;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
-
-// --- NEW UPDATE FUNCTION ---
-const updatePost = async (postId, updateData) => {
-    try {
-        const updatedPost = await Post.findByIdAndUpdate(
-            postId, 
-            updateData, 
-            { new: true, runValidators: true }
-        );
-        if (!updatedPost) {
-            throw new Error('Post not found');
-        }
-        return updatedPost;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+	try {
+		const posts = await postRepo.getPostsByUser(userId);
+		return posts;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
 }
 
 const deletePost = async (postId) => {
-    try {
-        const post = await Post.findByIdAndDelete(postId);
-        if (!post) {
-            throw new Error('Post not found');
-        }
-        return post;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+	try {
+		const deletedPost = await postRepo.deletePost(postId);
+		return deletedPost;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
+}
+
+const updatePost = async (postId, postData) => {
+	try {
+		const updatedPost = await postRepo.updatePost(postId, postData);
+		return updatedPost;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
 }
 
 const likePost = async (postId, userId) => {
-    try {
-        const post = await Post.findById(postId);
-        if (!post) {
-            throw new Error('Post not found');
-        }
-        if (post.likedBy.some(id => id.toString() === userId)) {
-            throw new Error('User has already liked this post');
-        }
-        post.likedBy.push(userId);
-        post.likes += 1;
-        await post.save();
-        return post;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+	try {
+		const likedPost = await postRepo.likePost(postId, userId);
+		return likedPost;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
 }
 
 const unlikePost = async (postId, userId) => {
-    try {
-        const post = await Post.findById(postId);
-        if (!post) {
-            throw new Error('Post not found');
-        }
-        const index = post.likedBy.findIndex(id => id.toString() === userId);
-        if (index === -1) {
-            throw new Error('User has not liked this post');
-        }
-        post.likedBy.splice(index, 1);
-        if (post.likes > 0) {
-            post.likes -= 1;
-        }
-        await post.save();
-        return post;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+	try {
+		const unlikedPost = await postRepo.unlikePost(postId, userId);
+		return unlikedPost;
+	} 
+	catch (error) {
+		throw new Error(error.message);
+	}
 }
 
 module.exports = {
-    getAllPosts,
-    getPostById,
-    getPostsByUser,
-    createPost,
-    updatePost, // Exporting new function
-    deletePost,
-    likePost,
-    unlikePost
+	createPost,
+	getPostById,
+	getAllPosts,
+	getPostsByUser,
+	deletePost,
+	updatePost,
+	likePost,
+	unlikePost
 }
