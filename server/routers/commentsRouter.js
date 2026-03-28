6a4aa449ff9ff7e4cacd96bfea67c7385873eea0
Aquib-cyber-stack <aquibs9999@gmail.com>
2026-03-28 10:39:15 +0530

    Update commentsRouter.js

--- server/routers/commentsRouter.js
+++ server/routers/commentsRouter.js
@@ -2,11 +2,12 @@ const commentsController = require('../controllers/commentController');
 const express = require('express');
 const { createCommentValidator } = require('../validators/commentValidator');
 const auth = require('../middlewares/auth');
-const validate= require('../middlewares/validate');
+const validate = require('../middlewares/validate');
 
 const router = express.Router();
 
-router.post('/:postId', createCommentValidator , auth , validate, commentsController.createComment);
+// ✅ FIX: auth runs FIRST, then validate, then controller
+router.post('/:postId', auth, createCommentValidator, validate, commentsController.createComment);
 router.get('/:postId', commentsController.getCommentsByPostId);
 
 module.exports = router;
